41371ff204c14f2b2495be6c6406b2f79073b34f
Emre CAN <i.e.can@yandex.com>
2023-03-28 01:47:21 +0300

    Sql task 11

---
+++ sqlOdev-11.sql
@@ -0,0 +1,58 @@
+-- actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+(
+SELECT first_name FROM actor
+)
+UNION
+(
+SELECT first_name FROM customer
+)
+ORDER BY first_name;
+
+-- actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+
+(
+SELECT first_name FROM actor
+)
+INTERSECT
+(
+SELECT first_name FROM customer
+)
+ORDER BY first_name;
+
+-- actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+(
+SELECT first_name FROM actor
+)
+EXCEPT
+(
+SELECT first_name FROM customer
+)
+ORDER BY first_name;
+
+-- İlk 3 sorguyu tekrar eden veriler için de yapalım.
+(
+SELECT first_name FROM actor
+)
+UNION
+(
+SELECT first_name FROM customer
+)
+ORDER BY first_name;
+
+(
+SELECT first_name FROM actor
+)
+INTERSECT
+(
+SELECT first_name FROM customer
+)
+ORDER BY first_name;
+
+(
+SELECT first_name FROM actor
+)
+EXCEPT
+(
+SELECT first_name FROM customer
+)
+ORDER BY first_name;
